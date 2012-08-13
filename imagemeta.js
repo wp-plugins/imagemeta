6fd309a4af2d20ba590efac3a86038c6bb76947a
era404 <era404@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-13 20:18:03 +0000

    Adding first version (v0.1) of ImageMeta plugin
    git-svn-id: https://plugins.svn.wordpress.org/imagemeta/trunk@585037 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ imagemeta.js
@@ -0,0 +1,72 @@
+/*
+ * imagemeta handlers
+*/
+
+	jQuery(document).ready(function() {
+    jQuery('#imagemetas input[type="text"]').addClass("idleField");  
+    jQuery('#imagemetas input[type="text"]').focus(function() {  
+        jQuery(this).removeClass("idleField").addClass("focusField");  
+        if (this.value == this.defaultValue){  
+            this.select(); 
+        }  
+
+    });  
+    jQuery('#imagemetas input[type="text"]').blur(function() {  
+        jQuery(this).removeClass("focusField").addClass("idleField");
+        var fd = jQuery(this);
+        if (jQuery.trim(fd.val()) == '' || fd.val() == unescape(this.defaultValue)){  
+            this.value = (this.defaultValue ? unescape(this.defaultValue) : '');  
+            //alert("default value or blank");
+        }
+        else
+        {	var fname = fd.attr('id').split(":");
+        	var fval = fd.val();
+        	//alert(fname + " > " + fval);
+        	updateField(fname,fval);
+        }
+    });  
+});
+
+function updateField(fname,fval){
+    showLoading(fname[0]+":"+fname[1]);  	// shows updating gif
+	jQuery.post(ajax_object.ajaxurl, {
+		action: 'ajax_action',
+		fval: fval,
+		fname: fname							// query is built in ajax function; returns true/false
+	}, function(data) {
+		//alert(data); 							// changes default value
+		document.getElementById(fname[0]+":"+fname[1]).defaultValue = escape(fval);
+		hideLoading(fname[0]+":"+fname[1]);	// hides updating gif
+	});
+	return;
+}
+
+// update indicators
+function showLoading(div){
+	if(document.getElementById(div)) {
+		document.getElementById(div).className = 'updateField';
+	}
+}
+function hideLoading(div){
+	if(document.getElementById(div)) {
+		document.getElementById(div).className = 'idleField';
+	}
+}
+// copy titles across fields
+function copyAcross(fID,mID){
+	fval = document.getElementById("post_title:"+fID).value;
+
+	var caption = document.getElementById('post_excerpt:'+fID);
+	if(caption) { caption.value = unescape(fval);
+				  updateField(["post_excerpt",fID],fval); }
+				  
+	var description = document.getElementById('post_content:'+fID);
+	if(description) { description.value = unescape(fval);
+					  updateField(["post_content",fID],fval); }
+					  
+	var alt = document.getElementById('meta_value:'+mID);
+	if(alt) { alt.value = unescape(fval);
+			  updateField(["meta_value",mID],fval); }
+	return;	
+}
+
---
+++ imagemeta.php
@@ -0,0 +1,395 @@
+<?php
+/*
+Plugin Name: ImageMeta
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: The fastest way to manage meta-data for your wordpress images.
+Version: 0.1
+Author: era404 Creative Group, Inc.
+Author URI: http://www.era404.com
+License: GPLv2 or later.
+Copyright 2012  era404 Creative Group, Inc.  (email : in4m@era404.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+//globals
+define('RECORDS_PER_PAGE', 50);
+define('IMAGEMETA_URL', admin_url() . 'options-general.php?page=imagemeta');
+
+// Setup plugin menus
+add_action( 'admin_menu', 'imagemeta_admin_menu' );
+function imagemeta_admin_menu() {
+	add_options_page( 'Image Meta', 'Image Meta', 'manage_options', 'imagemeta', 'imagemeta_plugin_options' );
+}
+
+// Setup plugin scripts and styles
+wp_enqueue_script( 'ajax-script', plugins_url('/imagemeta.js', __FILE__), array('jquery'), 1.0 ); // jQuery will be included automatically
+wp_localize_script( 'ajax-script', 'ajax_object', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) ); // setting ajaxurl
+add_action( 'wp_ajax_ajax_action', 'ajax_updatedb' ); 	//for updates
+
+
+// Build admin page
+function imagemeta_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	//get database object
+	global $wpdb;
+		
+	//handle deleting images
+	$cwpp = 0; $cwppm = 0;
+	if(isset($_GET['remove'])) {
+		$qwpp = "SELECT count(ID) FROM {$wpdb->prefix}posts WHERE ID={$_GET['remove']}"; $cwpp = $wpdb->get_var($qwpp);
+		$rwpp = "DELETE FROM {$wpdb->prefix}posts WHERE ID={$_GET['remove']}"; $wpdb->query($rwpp);
+		
+		$qwppm = "SELECT count(meta_id) FROM {$wpdb->prefix}postmeta WHERE post_id={$_GET['remove']}"; $cwppm = $wpdb->get_var($qwppm);
+		$rwppm = "DELETE FROM {$wpdb->prefix}postmeta WHERE post_id={$_GET['remove']}"; $wpdb->query($rwppm);
+	}
+	
+
+	//handle sorting
+	$sorting = array("d"=>"post_date ASC",
+					 "d-"=>"post_date DESC",
+					 "t"=>"post_title ASC",
+					 "t-"=>"post_title DESC");
+
+	$s = $_GET['s'] = (@!isset($_GET['s']) || !array_key_exists($_GET['s'],$sorting) ? "d" : $_GET['s']);
+	$sort = $sorting[$_GET['s']];
+		
+
+	
+	//get count, first
+	$cq =  "SELECT ID from {$wpdb->prefix}posts, {$wpdb->prefix}postmeta
+			WHERE {$wpdb->prefix}postmeta.post_ID = {$wpdb->prefix}posts.ID
+			AND {$wpdb->prefix}postmeta.meta_key ='_wp_attached_file'
+			GROUP BY {$wpdb->prefix}posts.ID";
+	$crs = $wpdb->get_results($cq, ARRAY_A);
+	$count = count($crs);
+	//echo "COUNT: ".count($crs)."<br /><br />";
+	
+	/* build page array [$pg]
+	*  0:total records
+	*  1:records per page << defined above
+	*  2:total pages
+	*  3:current page (also:$p)
+	*  4:record start
+	*  5:record end
+	*/
+	
+	$pg = array($count,RECORDS_PER_PAGE,ceil($count/RECORDS_PER_PAGE));
+	$p = $_GET['p'] = $pg[3] = (!isset($_GET['p']) || $_GET['p']<1 || $_GET['p']>$pg[2] ? 1 : $_GET['p']);
+	$pg[4] = ($pg[1]*$pg[3])-$pg[1]; 
+	$pg[5]=(($pg[4]+$pg[1])-1);
+			
+	//build query
+	$q =   "SELECT  {$wpdb->prefix}posts.ID as postid, 
+					{$wpdb->prefix}posts.post_content, 
+					{$wpdb->prefix}posts.post_title, 
+					{$wpdb->prefix}posts.post_excerpt, 
+					{$wpdb->prefix}posts.post_date, 
+					{$wpdb->prefix}postmeta.meta_value as img,
+	
+			(SELECT meta_value FROM {$wpdb->prefix}postmeta WHERE post_ID = postid AND meta_key='_wp_attachment_image_alt' ORDER BY meta_id DESC LIMIT 1) AS alt,
+			(SELECT meta_id FROM {$wpdb->prefix}postmeta WHERE post_ID = postid AND meta_key='_wp_attachment_image_alt' ORDER BY meta_id DESC LIMIT 1) AS meta_id
+			
+			FROM 	{$wpdb->prefix}posts,{$wpdb->prefix}postmeta
+			WHERE 	{$wpdb->prefix}postmeta.post_ID = {$wpdb->prefix}posts.ID
+			AND 	{$wpdb->prefix}postmeta.meta_key = '_wp_attached_file'
+			GROUP BY {$wpdb->prefix}postmeta.meta_id
+			ORDER BY {$wpdb->prefix}posts.{$sort}
+			LIMIT {$pg[4]},{$pg[5]}
+			"; 
+			//echo $q;
+	
+	//verify all images have a paired meta field / create record otherwise
+	$imgs = $wpdb->get_results($q, ARRAY_A);
+
+	foreach($imgs as $k=>$i) {
+		if($i['meta_id']=="") {
+			$wpdb->query("INSERT INTO {$wpdb->prefix}postmeta (meta_id,post_id,meta_key,meta_value) VALUES (NULL,{$i['postid']},'_wp_attachment_image_alt','')");
+			}
+	}
+
+	//pull 30 rows of images
+	$imgs = $wpdb->get_results($q, ARRAY_A);
+	
+
+	//myprint_r($imgs);
+	addstyles();
+	
+	//build images table
+	echo "<h1>Image Meta</h1><br />
+		  A very light-weight plugin, designed to make updating meta-values (title, caption, description and alternate text) more efficient, by providing one central table of imagery and a duplicate button to copy this information across fields. <b>Note: </b>Updates are performed immediately after defocusing a field.<br /><br />";
+	
+	// handle pager
+	echo "<div style='clear:both;'>&nbsp; Page: $p (".($pg[4]+1)." - ".($pg[5]+1)." of {$pg[0]}) ";
+	for($page=1;$page<=$pg[2];$page++){ echo "<p class='pager'><a href='".IMAGEMETA_URL."&p={$page}&s={$s}'>{$page}</a></p>"; }
+	echo "</div>
+	<br /><br />";
+	
+	echo "<div id='warning' class='orange'>".(($cwpp>1||$cwppm>1)?"NOTE:<br />($cwpp) post records removed.<br />($cwppm) postmeta records removed.<br /><br />":"")."</div>";
+	echo "<table id='imagemetas'>";
+	echo "<thead id='imagemetahead'><tr><th>&nbsp;</th>
+			<th>
+				<a href='".IMAGEMETA_URL."&s=t' title='Sort by Title A-Z' class='sort_a ".($_GET['s']=='t'?'selected':'')."'><span>Ascending</span></a>
+				Title
+				<a href='".IMAGEMETA_URL."&s=t-' title='Sort by Title Z-A' class='sort_d ".($_GET['s']=='t-'?'selected':'')."'><span>Descending</span></a>
+			</th>
+			<th>&nbsp;</th>
+			<th>
+				<a href='".IMAGEMETA_URL."&s=d' title='Sort by Date Ascending' class='sort_a ".($_GET['s']=='d'?'selected':'')."'><span>Ascending</span></a>
+				Date
+				<a href='".IMAGEMETA_URL."&s=d-' title='Sort by Date Descending' class='sort_d ".($_GET['s']=='d-'?'selected':'')."'><span>Descending</span></a>
+			</th>
+		</tr></thead>";
+	
+	$missing = 0;
+	foreach($imgs as $k=>$i){
+		$lg = ( wp_get_attachment_image_src( $i['postid'], 'large' ) );
+		$sm = ( wp_get_attachment_image_src( $i['postid'], 'thumbnail' ) );
+		$cp = plugins_url('/copyacross.gif', __FILE__);
+		$mia = plugins_url('/missing.png', __FILE__);
+		$rm = plugins_url('/delete.png', __FILE__);
+		$ed = plugins_url('/edit.png', __FILE__);
+		$admin = admin_url(); 
+		$date = date("m/d/Y",strtotime($i['post_date']));
+		
+		$contentdir = explode("/", WP_CONTENT_DIR);						  //get content folder name
+		$filesplit = explode($contentdir[(count($contentdir)-1)],$sm[0]); //get image file path
+
+		if(file_exists(WP_CONTENT_DIR . $filesplit[1])) {
+			$link = "<a href='{$lg[0]}' target='_blank'><img src='{$sm[0]}' width='60'></a>";
+			$rmStyle = "";
+			$del = "";
+		} else {
+			$link = "<img src='{$mia}' />";
+			$del = "<a href='" . IMAGEMETA_URL . "&remove={$i['postid']}' onclick='javascript:if(!confirm(\"Are you sure you want to move this item to trash?\")) return false;' title='Permanently Delete This Image' target='_self' /><img src='{$rm}' width='16' height='16' align='absmiddle' /></a>&nbsp;";
+			//echo WP_CONTENT_DIR . $filesplit[1] . " could not be found.<br />";
+			$rmStyle = "orange"; $missing++;
+		}
+		
+
+
+echo <<<EOHTML
+	<tr class='info $rmStyle'>
+		<td colspan='4'><div style='float: right;'><a href='{$admin}media.php?attachment_id={$i['postid']}&action=edit' title='Edit This Item' target='_blank'><img src="$ed" width='16' height=16' align='absmiddle' /></a>&nbsp;&nbsp;{$del}</div><b>$date</b>: {$filesplit[1]}</td>
+	</tr>
+	<tr class='row'>
+		<td><div class='thumb'>{$link}</div></td>
+		<td>
+			<p>Title:</p>	 	 <input type='text' id='post_title:{$i['postid']}' value='{$i['post_title']}' />
+			<p>Caption:</p> 	 <input type='text' id='post_excerpt:{$i['postid']}' value='{$i['post_excerpt']}' />			
+		</td>
+		<td width='30' valign='top'><a href='javascript:void(0);' onclick='javascript:copyAcross({$i['postid']},{$i['meta_id']});' title='Copy Title Across'><img src='{$cp}' alt='Copy Title Across' border='0' class='copyAcross' /></a> </td>
+		<td>
+			<p>Descr:</p>  <input type='text' id='post_content:{$i['postid']}' value='{$i['post_content']}' />
+			<p>Alt:</p>    <input type='text' id='meta_value:{$i['meta_id']}' value='{$i['alt']}' />
+		</td>
+	</tr>
+EOHTML;
+	}
+	echo "</table>";
+
+	if($missing>0) { echo <<<EOWARNING
+<script type='text/javascript'>
+var warning = document.getElementById('warning');
+warning.innerHTML += "WARNING:<br />($missing) image files do not appear to exist in your content folder.<br />You can delete these images individually using the links below.";
+</script>
+EOWARNING;
+	}
+}
+
+
+/**************************************************************************************************
+*	Some useful functions
+**************************************************************************************************/
+
+
+function ajax_updatedb() {
+	global $wpdb;
+	
+	//build query from passed vars
+	$fval = $_POST['fval'];
+	$fname = $_POST['fname'];
+	$q = "UPDATE ".(substr($fname[0],0,4)=="post"?"{$wpdb->prefix}posts":"{$wpdb->prefix}postmeta").
+		" SET {$fname[0]} = '{$fval}' WHERE ".
+		(substr($fname[0],0,4)=="post"?"ID":"meta_id")." = {$fname[1]}";
+		
+	print_r($_POST); echo "Query: $q";
+	
+	$wpdb->query($q);
+	die(); // stop executing script
+}
+function myprint_r($in) {
+	echo "<pre>"; print_r($in); echo "</pre>"; return;
+}
+function addstyles() {
+	echo <<<STYLES
+	<style type="text/css">
+    *{
+        margin:0;
+        padding:0;
+        font-family: "Lucida Grande", Arial, sans-serif;
+    }
+    #imagemetas p {
+    	width:60px;
+    	overflow:hidden;
+    	margin:0;
+    	float:left;
+    	font-weight:bold;
+    }
+	#imagemetas {
+		border-collapse: collapse;
+		border-spacing: 0;
+		width: 100%;
+	}
+    #imagemetas tr.info td {
+		padding: 5px;
+		font-size: 14px;
+    }
+
+	tr:nth-child(2n+1){
+	  background-color: #f0f0f0;
+	}
+
+	#imagemetahead tr, #imagemetahead tr th {
+		height: 20px;
+		background-color: #cfcfcf;
+		font-weight: bold;
+	}
+	.sort_a, .sort_d {
+		width: 9px;
+		height: 9px;
+		display: inline-block;
+		margin: 0 2px;
+	}
+	.sort_a span, .sort_d span {
+		display: none;
+	}
+	.sort_a {
+		background-position: 9px 0;
+	}
+	.sort_d {
+		background-position: 9px 9px;
+	}
+	.sort_a.selected {
+		background-position: 0 0;
+	}
+	.sort_d.selected {
+		background-position: 0 9px;
+	}
+		.sort_a:hover {
+		background-position: 18px 0;
+	}
+	.sort_d:hover {
+		background-position: 18px 9px;
+	}
+    .thumb {
+    	height:45px;
+    	width:60px;
+    	overflow:hidden;
+    	margin-right:10px;
+    }
+    body {
+        padding: 10px;
+    }
+    #status {
+        width:50%;
+        padding:10px;
+        outline:none;
+        height:36px;
+    }
+    .pager {
+    	width:18px;
+    	height:14px;
+    	background-color:#DDD;
+    	border:1px solid #AAA;
+    	display:block;
+    	text-decoration: none;
+    	text-align:center;
+    	float:left;
+		margin: 0 3px;
+    }
+	.pager a {
+		text-decoration:none;
+	}
+	.pager a:hover {
+		font-weight:bold;
+	}
+    .copyAcross {
+    	margin-top:10px;
+    }
+	tr.row {
+		height: 70px;
+	}
+	tr.orange {
+		background: #ffe4db;
+	}
+	#warning {
+		padding: 10px;
+		margin-bottom: 10px;
+	}
+	.focusField {
+		padding-left: 20px;
+		width: 300px;
+        border:solid 2px #73A6FF;
+        background:#EFF5FF;
+        color:#000;
+    }
+    .idleField {
+		padding-left: 20px;
+        width: 300px;
+		background:#EEE;
+        color: #6F6F6F;
+        border: solid 2px #DFDFDF;
+    }
+STYLES;
+echo "
+	.sort_a {
+		background-image: url(".plugins_url('/sort.gif', __FILE__).");
+	}
+	.sort_d {
+		background-image: url(".plugins_url('/sort.gif', __FILE__).");
+	}
+    .upd {
+    	margin:0;
+    	padding:0;
+    	float:left;
+    	width:16px;
+    	height:16px;
+    }
+    .updA {
+    	background-image: url(".plugins_url('/updating.gif', __FILE__).");
+    	margin:0;
+    	padding:0;
+    	float:left;
+    	width:16px;
+    	height:16px;
+    }
+	.focusField {
+		background: url(".plugins_url('/edit.png', __FILE__).") no-repeat 2px 2px;
+	}
+	.idleField {
+		background: url(".plugins_url('/okay.png', __FILE__).") no-repeat 2px 2px;
+	}
+	.updateField {
+		padding-left: 20px;
+		 width: 300px;
+		background: url(".plugins_url('/updating.gif', __FILE__).") no-repeat 2px 2px;
+	}
+</style>
+";
+return;
+}
+?>
